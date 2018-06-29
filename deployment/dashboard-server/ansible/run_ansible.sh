c4c51627bdeb0e3e1be88fc1d4ed01dbdd475e9d
Ed Tan <edward.tan1@gmail.com>
2018-06-29 11:40:30 -0400

    Fix sudo bugs in ansible scripts, and add secrets.yml for ease of maintenance

--- deployment/dashboard-server/ansible/run_ansible.sh
+++ deployment/dashboard-server/ansible/run_ansible.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ansible-playbook -i ./hosts site.yml --private-key ~/.ssh/techorrectAdmin.pem
+ansible-playbook -i ./hosts site.yml --private-key ~/.ssh/techorrectAdmin.pem --skip-tags="seed-data" -e @roles/dashboard/vars/secrets.yml
