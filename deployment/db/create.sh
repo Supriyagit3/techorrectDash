f809c7fa20cb9ee4feaedc4ab9bba19ce895c86c
Jeremy Lundy <jeremy.lundy@gmail.com>
2018-06-29 12:59:54 -0400

    Bind private IP in mongo config file and properly setup dashboard admin dbOwner role

--- deployment/db/create.sh
+++ deployment/db/create.sh
@@ -4,9 +4,10 @@ cd terraform
 yes yes | terraform apply
 
 PUBLIC_DNS=$(terraform output instance-dns)
+PRIVATE_IP=$(terraform output instance-private-ip)
 
-cat ../ansible/hosts.template | sed 's@<public_dns>@'"$PUBLIC_DNS"'@g' > ../ansible/hosts
+cat ../ansible/hosts.template | sed 's@<public_dns>@'"$PUBLIC_DNS"'@g;s@<private_ip>@'"$PRIVATE_IP"'@g' > ../ansible/hosts
 
 cd ../ansible
 
-yes yes | ansible-playbook -i ./hosts site.yml
+yes yes | ansible-playbook -i ./hosts site.yml --extra-vars "@secrets.yaml"
