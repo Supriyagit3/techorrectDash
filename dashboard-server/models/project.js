b5bbc5b654388726dbff039d975667947f86e537
Jeremy Lundy <jeremy.lundy@gmail.com>
2018-07-12 14:26:42 -0400

    Forgot to add the getSuiteCounts function to the projects model

--- dashboard-server/models/project.js
+++ dashboard-server/models/project.js
@@ -26,6 +26,32 @@ var projectSchema = new Schema(
   }
 );
 
+projectSchema.method("getSuiteCounts", function(cb) {
+  thisFailedLvl = this.healthyFailedLevel;
+  thisSkippedLvl = this.healthySkippedLevel;
+  thisDisabledLvl = this.healthyDisabledLevel;
+  Suite.find({ projectId: this._id }).exec(function(err, suites) {
+    if (err) throw err;
+
+    var unhealthyCounts = 0;
+    var healthyCounts = 0;
+
+    suites.forEach(function(suite) {
+      if (
+        suite.failingTests < thisFailedLvl &&
+        suite.skippedTests < thisSkippedLvl &&
+        suite.disabledTests < thisDisabledLvl
+      )
+        healthyCounts++;
+      else {
+        unhealthyCounts++;
+      }
+    });
+
+    return cb(unhealthyCounts, healthyCounts);
+  });
+});
+
 var Project = mongoose.model("Project", projectSchema);
 
 module.exports = Project;
